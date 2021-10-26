8d1725baf829b6ce27e45d82d56b586d7f899163
alejandroVaron <alejandro.varon@correounivalle.edu.co>
2021-10-26 12:46:54 -0500

    implement express

---
+++ back-end/src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import urlShortener from 'node-url-shortener';
+import cors from 'cors';
+
+const app = express();
+var port =   process.env.PORT || 3000;
+if(!process.env.DATABASE_URL){
+    port = 3000;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.end('¡Welcome!');
+})
+
+app.post('/url', function(req, res) {
+    const url = req.body.url;
+    urlShortener.short(url, function(err, shortUrl){
+        res.send(shortUrl);
+    });
+});
+
+app.listen(port, function(){
+    console.log('¡Server up in port '+port+'!');
+});
+
+/*
+sequelize.sync({ force: false, logging: console.log }).then( () => {
+    console.log("¡We connect to the database!");
+}).catch((error) => {
+    console.log(error)
+});
+*/
